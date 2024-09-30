7a75cbf902a7b0a4e78768bcb983937c902c8d05
ezool <lys@ezool.cn>
2024-09-30 14:14:12 +0800

    修改为自己的域名

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import DOCS from './help.html'
+
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
+
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,17 +20,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.7x.pub": dockerHub,
+  "quay.7x.pub": "https://quay.io",
+  "gcr.7x.pub": "https://gcr.io",
+  "k8s-gcr.7x.pub": "https://k8s.gcr.io",
+  "k8s.7x.pub": "https://registry.k8s.io",
+  "ghcr.7x.pub": "https://ghcr.io",
+  "cloudsmith.7x.pub": "https://docker.cloudsmith.io",
+  "ecr.7x.pub": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.7x.pub": dockerHub,
 };
 
 function routeByHosts(host) {
